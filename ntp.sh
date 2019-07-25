943b29f2c6e9033e1a25123b785918cd065013e4
Jamshid <apple@Apples-MBP.localdomain>
2019-07-25 01:25:15 -0400

    Create ntp.sh

---
+++ ntp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+yum install -y ntp
+clear
+echo "server 192.168.46.60 prefer" >> /etc/ntp.conf
+firewall-cmd --add-service=ntp --permanent
+firewall-cmd --add-port=123/udp --permanent
+firewall-cmd --reload
+stemctl enable ntpd
+systemctl start ntpd
+timedatectl
+ntpq -p
